f9f3c4d692119517754dbb485d4498671711ba24
JAMES CARVALLYO <jpcarvallyo@gmail.com>
2023-04-25 08:23:29 -0700

    Move server code into server directory

--- server/src/resources/index.js
+++ server/src/resources/index.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 
 const dictionary = JSON.parse(
-  fs.readFileSync("src/resources/dictionary.json", "utf8")
+  fs.readFileSync("server/src/resources/dictionary.json", "utf8")
 );
 
 const rhymesWithDictionary = JSON.parse(
-  fs.readFileSync("src/resources/rhymesWith.json", "utf8")
+  fs.readFileSync("server/src/resources/rhymesWith.json", "utf8")
 );
 
 const urbanDictionary = JSON.parse(
-  fs.readFileSync("src/resources/urbanDictionaryA5.json", "utf-8")
+  fs.readFileSync("server/src/resources/urbanDictionaryA5.json", "utf-8")
 );
 
 const wordList = Object.keys(dictionary);
