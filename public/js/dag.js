4d92253f78075c1175230cf599a66c211b53f5f3
JAMES CARVALLYO <jpcarvallyo@gmail.com>
2023-05-06 16:55:18 -0700

    Add DAG current price to UI and fix styles

--- public/js/dag.js
+++ public/js/dag.js
@@ -2,12 +2,14 @@ import { getCurrentDagPrice, priceOfDagNode } from "./utils/index.js";
 const currentPrice = document.querySelector("h2#currentPrice");
 const inputPrice = document.querySelector("input#customPriceInput");
 const customPrice = document.querySelector("h2#customPrice");
+const dagCurrentPrice = document.querySelector("h2#dagCurrentPrice");
 const submitBtn = document.querySelector("button");
 
 const getPrice = async () => {
   const price = await getCurrentDagPrice();
   const nodePrice = await priceOfDagNode(price);
   currentPrice.textContent = `$${nodePrice.toLocaleString()}`;
+  dagCurrentPrice.textContent = `$${price.toLocaleString()}`;
 };
 
 const setCustomPrice = () => {
